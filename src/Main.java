3b9390d922b1d75fcc470c5371b84729a5c299a0
ABEL SAGA <68258483+sg-abel@users.noreply.github.com>
2024-06-25 09:47:57 -0600

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+       var nombre = "Odette";
+
+        System.out.println("nueva linea: \n" + nombre); //salto de linea
+        System.out.println("tabulador: \t" + nombre);
+        System.out.println("retroceso: \b\b" + nombre);
+        System.out.println("comilla simple: \'" + nombre + "\'");
+        System.out.println("comilla doble: \"" + nombre + "\"");
+
+    }
+}
